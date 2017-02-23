3a30c401745aac724f50a2f839b088cfbea2f144
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-02-23 14:46:30 -0700

    finished through step 3

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var config = require('./.config.js');
+var profileCtrl = require('./controllers/profileCtrl.js');
+var userCtrl = require('./controllers/userCtrl.js');
+var session = require('express-session')
+
 
 var app = express();
 var port = 3000;
@@ -9,9 +13,19 @@ var corsOptions = {
     origin: 'http://localhost:3000'
 };
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors(corsOptions));
+app.use(session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+app.post('/api/login', userCtrl.login);
+app.get('/api/profiles', profileCtrl.friends);
 
 
 
